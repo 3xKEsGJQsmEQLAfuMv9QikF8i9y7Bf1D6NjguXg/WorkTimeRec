9555527f3bc9b3b843ee83d1b146d92dedf24aea
3xKEsGJQsmEQLAfuMv9QikF8i9y7Bf1D6NjguXg <99333667+3xKEsGJQsmEQLAfuMv9QikF8i9y7Bf1D6NjguXg@users.noreply.github.com>
2022-03-16 12:45:37 +0900

    v1.0.1.1

---
+++ WorkTimeRec/WorkTimeRec/ユーティリティ/処理制御.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace WorkTimeRec.ユーティリティ
+{
+    internal class 処理制御
+    {
+        private int _実行回数 = 0;
+        private readonly Action _処理;
+
+        public 処理制御(Action 処理)
+        {
+            _処理 = 処理;
+        }
+
+        public void 一回実行()
+        {
+            if (0 < _実行回数)
+            {
+                return;
+            }
+
+            _処理();
+            _実行回数++;
+        }
+
+    }
+}
