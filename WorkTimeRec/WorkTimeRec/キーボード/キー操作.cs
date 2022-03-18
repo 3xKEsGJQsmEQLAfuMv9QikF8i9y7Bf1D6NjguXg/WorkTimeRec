48ba189ac9516716e2cce8a17a8caa78905e0a46
3xKEsGJQsmEQLAfuMv9QikF8i9y7Bf1D6NjguXg <99333667+3xKEsGJQsmEQLAfuMv9QikF8i9y7Bf1D6NjguXg@users.noreply.github.com>
2022-03-18 20:36:28 +0900

    v1.0.2.0

--- WorkTimeRec/WorkTimeRec/キーボード/キー操作.cs
+++ WorkTimeRec/WorkTimeRec/キーボード/キー操作.cs
@@ -4,9 +4,13 @@ namespace WorkTimeRec.キーボード
 {
     internal class キー操作
     {
-        public static bool Ctrlキー押下()
-        {
-            return Keyboard.Modifiers == ModifierKeys.Control;
-        }
+        public static bool Ctrlキー押下 =>
+            Keyboard.Modifiers == ModifierKeys.Control;
+
+        public static bool Shiftキー押下 =>
+            Keyboard.Modifiers == ModifierKeys.Shift;
+
+        public static bool CtrlとShiftキー押下 =>
+            Keyboard.Modifiers == (ModifierKeys.Control | ModifierKeys.Shift);
     }
 }
---
+++ WorkTimeRec/WorkTimeRec/コントロールロジック/リストビュー操作.cs
@@ -0,0 +1,44 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace WorkTimeRec.コントロールロジック
+{
+    internal class リストビュー操作
+    {
+        public static void 最初のリスト登録時の表示設定(ListView list)
+        {
+            if (list.Opacity != 0.9)
+            {
+                list.Opacity = 0.9;
+            }
+        }
+
+        public static void サイズ変更(object sender, SizeChangedEventArgs e)
+        {
+            if (!(sender is ListView lv))
+            {
+                return;
+            }
+
+            if (!(lv.View is GridView v))
+            {
+                return;
+            }
+
+            const double MarginW = 18.0;
+            const double MinW = 75.0;
+
+            var w = lv.ActualWidth
+                - v.Columns[0].ActualWidth
+                - v.Columns[1].ActualWidth
+                - v.Columns[2].ActualWidth
+                - MarginW;
+            if (w < MinW)
+            {
+                w = MinW;
+            }
+            v.Columns[3].Width = w;
+        }
+    }
+
+}
