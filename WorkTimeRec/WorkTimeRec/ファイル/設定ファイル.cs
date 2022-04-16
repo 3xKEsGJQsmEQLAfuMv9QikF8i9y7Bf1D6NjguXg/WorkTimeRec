9a466dcdcd098d64e5395ce8c3c3c22118e27978
3xKEsGJQsmEQLAfuMv9QikF8i9y7Bf1D6NjguXg <99333667+3xKEsGJQsmEQLAfuMv9QikF8i9y7Bf1D6NjguXg@users.noreply.github.com>
2022-04-16 20:42:42 +0900

    v1.0.5.0

--- WorkTimeRec/WorkTimeRec/ファイル/設定ファイル.cs
+++ WorkTimeRec/WorkTimeRec/ファイル/設定ファイル.cs
@@ -6,10 +6,32 @@ namespace WorkTimeRec.ファイル
 {
     internal class 設定ファイル
     {
+        private static readonly string[] _settingNames = 
+        {
+            "TaskbarIcon",
+            "TaskbarColor",
+            "WorkingComboState",
+            "RestoreComboText",
+            "ParallelSave",
+            "Parallel",
+            "StopConfirm",
+            "Notify1",
+            "NotifyTime1",
+            "NotifyMsg1",
+            "Notify2",
+            "NotifyTime2",
+            "NotifyMsg2",
+            "Notify3",
+            "NotifyTime3",
+            "NotifyMsg3",
+        };
+
         public static 設定 読み込み()
         {
             var cfg = ConfigurationManager.OpenExeConfiguration(
                 ConfigurationUserLevel.None);
+
+            設定ファイル項目存在チェック(cfg);
 
             var cfgData = new 設定(
                 タスクバーアイコン: GetEnum<作業中タスクバーアイコン>(GetInt(cfg.AppSettings.Settings["TaskbarIcon"].Value, 0)),
@@ -18,10 +40,50 @@ namespace WorkTimeRec.ファイル
                 起動時に作業コンボボックスのテキスト設定: GetBool(cfg.AppSettings.Settings["RestoreComboText"].Value, true),
                 並行作業保存: GetBool(cfg.AppSettings.Settings["ParallelSave"].Value, false),
                 並行作業: GetBool(cfg.AppSettings.Settings["Parallel"].Value, false),
-                作業終了の確認: GetBool(cfg.AppSettings.Settings["StopConfirm"].Value, false)
-                );
+                作業終了の確認: GetBool(cfg.AppSettings.Settings["StopConfirm"].Value, false),
+                通知: new 通知情報[]
+                {
+                    new(
+                        GetBool(cfg.AppSettings.Settings["Notify1"].Value, false),
+                        GetDateTime(cfg.AppSettings.Settings["NotifyTime1"].Value, DateTime.MinValue),
+                        cfg.AppSettings.Settings["NotifyMsg1"].Value
+                    ),
+                    new(
+                    GetBool(cfg.AppSettings.Settings["Notify2"].Value, false),
+                    GetDateTime(cfg.AppSettings.Settings["NotifyTime2"].Value, DateTime.MinValue),
+                    cfg.AppSettings.Settings["NotifyMsg2"].Value
+                    ),
+                    new(
+                    GetBool(cfg.AppSettings.Settings["Notify3"].Value, false),
+                    GetDateTime(cfg.AppSettings.Settings["NotifyTime3"].Value, DateTime.MinValue),
+                    cfg.AppSettings.Settings["NotifyMsg3"].Value
+                    )
+                }
+            );
 
             return cfgData;
+        }
+
+        private static void 設定ファイル項目存在チェック(Configuration? cfg)
+        {
+            if (cfg is null)
+            {
+                return;
+            }
+
+            string key = "";
+            try
+            {
+                foreach (string k in _settingNames)
+                {
+                    key = k;
+                    _ = cfg.AppSettings.Settings[key].Value;
+                }
+            }
+            catch
+            {
+                throw new FormatException($"キー「{key}」が見つかりません。");
+            }
         }
 
         public static void 保存(設定 cfgData)
@@ -37,26 +99,41 @@ namespace WorkTimeRec.ファイル
             cfg.AppSettings.Settings["Parallel"].Value = cfgData.並行作業.ToString();
             cfg.AppSettings.Settings["StopConfirm"].Value = cfgData.作業終了の確認.ToString();
 
+            for (int i = 0; i < cfgData.通知.Length; i++)
+            {
+                cfg.AppSettings.Settings[$"Notify{i+1}"].Value = cfgData.通知[i].通知表示.ToString();
+                cfg.AppSettings.Settings[$"NotifyTime{i+1}"].Value = cfgData.通知[i].通知時刻.ToString("HH:mm");
+                cfg.AppSettings.Settings[$"NotifyMsg{i+1}"].Value = cfgData.通知[i].メッセージ.ToString();
+            }
+
             cfg.Save();
         }
 
-        public static int GetInt(string value, int defalutValue)
+        public static int GetInt(string value, int defaultValue)
         {
             return int.TryParse(value, out int v)
                 ? v
-                : defalutValue;
+                : defaultValue;
         }
 
-        public static bool GetBool(string value, bool defalutValue)
+        public static bool GetBool(string value, bool defaultValue)
         {
             return bool.TryParse(value, out bool b)
                 ? b
-                : defalutValue;
+                : defaultValue;
         }
 
         public static T GetEnum<T>(int value)
         {
             return (T)Enum.ToObject(typeof(T), value);
+        }
+
+        public static DateTime GetDateTime(string value, DateTime defaultValue)
+        {
+            return DateTime.TryParse(value, out DateTime d)
+                ? d
+                : defaultValue;
+
         }
 
     }
